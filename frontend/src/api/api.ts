a45716f0deec7accea91082469995c9e3c56daf6
Sarah Widder <sawidder@microsoft.com>
2023-06-08 15:21:21 -0700

    Revert "Add requirement for identity provider."
    This reverts commit 496f702952b0e315530e5924fefb3b12ae73ea5b.

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,4 +1,4 @@
-import { UserInfo, ConversationRequest } from "./models";
+import { ChatResponse, ConversationRequest } from "./models";
 
 export async function conversationApi(options: ConversationRequest, abortSignal: AbortSignal): Promise<Response> {
 ***REMOVED***const response = await fetch("/conversation", {
@@ -13,15 +13,4 @@ export async function conversationApi(options: ConversationRequest, abortSignal:
 ***REMOVED***);
 
 ***REMOVED***return response;
-}
-
-export async function getUserInfo(): Promise<UserInfo[]> {
-***REMOVED***const response = await fetch('/.auth/me');
-***REMOVED***if (!response.ok) {
-***REMOVED***console.log("No identity provider found. Access to chat will be blocked.")
-***REMOVED***return [];
-***REMOVED***
-
-***REMOVED***const payload = await response.json();
-***REMOVED***return payload;
 }
