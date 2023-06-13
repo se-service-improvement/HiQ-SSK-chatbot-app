d80a20fd17a80d0c1d8620d40983224e69a71ab7
Pamela Fox <pamela.fox@gmail.com>
2023-06-13 18:11:43 +0000

    Update scripts to use requirements-dev.txt

--- scripts/auth_init.py
+++ scripts/auth_init.py
@@ -1,46 +1,89 @@
+import argparse
 import subprocess
 
 from azure.identity import AzureDeveloperCliCredential
 import urllib3
 
-credential = AzureDeveloperCliCredential()
+def get_auth_headers(credential):
+***REMOVED***return {
+***REMOVED***"Authorization": "Bearer "
+***REMOVED******REMOVED***+ credential.get_token("https://graph.microsoft.com/.default").token
+***REMOVED***
 
-resp = urllib3.request(
+def check_for_application(credential, app_id):
+***REMOVED***resp = urllib3.request(
+***REMOVED***"GET",
+***REMOVED***f"https://graph.microsoft.com/v1.0/applications/{app_id}",
+***REMOVED***headers=get_auth_headers(credential))
+***REMOVED***if resp.status != 200:
+***REMOVED***print("Application not found")
+***REMOVED***return False
+***REMOVED***return True
+
+
+def create_application(credential):
+***REMOVED***resp = urllib3.request(
 ***REMOVED***"POST",
 ***REMOVED***"https://graph.microsoft.com/v1.0/applications",
-***REMOVED***headers={
-***REMOVED***"Authorization": "Bearer "
-***REMOVED***+ credential.get_token("https://graph.microsoft.com/.default").token,
-***REMOVED***,
+***REMOVED***headers=get_auth_headers(credential),
 ***REMOVED***json={
-***REMOVED***"displayName": "WebApp",
-***REMOVED***"signInAudience": "AzureADandPersonalMicrosoftAccount",
-***REMOVED***"web": {"redirectUris": ["http://localhost:5000/.auth/login/aad/callback"],
-***REMOVED******REMOVED***"implicitGrantSettings": {
+***REMOVED******REMOVED***"displayName": "WebApp",
+***REMOVED******REMOVED***"signInAudience": "AzureADandPersonalMicrosoftAccount",
+***REMOVED******REMOVED***"web": {"redirectUris": ["http://localhost:5000/.auth/login/aad/callback"],
+***REMOVED******REMOVED******REMOVED***"implicitGrantSettings": {
 ***REMOVED******REMOVED***"enableIdTokenIssuance": True
 ***REMOVED***},
 ***REMOVED***,
 ***REMOVED***timeout=urllib3.Timeout(connect=10, read=10),
-)
+***REMOVED***)
 
-app_id = resp.json()["id"]
-client_id = resp.json()["appId"]
+***REMOVED***app_id = resp.json()["id"]
+***REMOVED***client_id = resp.json()["appId"]
 
-# Add a client secret to the application
-# using https://graph.microsoft.com/v1.0/applications/{id}/addPassword
-# where {id} is the application ID returned from the previous step
-resp = urllib3.request(
+***REMOVED***return app_id, client_id
+
+def add_client_secret(credential, app_id):
+***REMOVED***resp = urllib3.request(
 ***REMOVED***"POST",
 ***REMOVED***f"https://graph.microsoft.com/v1.0/applications/{app_id}/addPassword",
-***REMOVED***headers={
-***REMOVED***"Authorization": "Bearer "
-***REMOVED***+ credential.get_token("https://graph.microsoft.com/.default").token,
-***REMOVED***,
+***REMOVED***headers=get_auth_headers(credential),
 ***REMOVED***json={"passwordCredential": {"displayName": "WebAppSecret"}},
 ***REMOVED***timeout=urllib3.Timeout(connect=10, read=10),
-)
-client_secret = resp.json()["secretText"]
+***REMOVED***)
+***REMOVED***client_secret = resp.json()["secretText"]
+***REMOVED***return client_secret
 
-subprocess.run(f"azd env set AUTH_APP_ID {app_id}", shell=True)
-subprocess.run(f"azd env set AUTH_CLIENT_ID {client_id}", shell=True)
-subprocess.run(f"azd env set AUTH_CLIENT_SECRET {client_secret}", shell=True)
+def update_azd_env(name, val):
+***REMOVED***subprocess.run(f"azd env set {name} {val}", shell=True)
+
+if __name__ == "__main__":
+***REMOVED***parser = argparse.ArgumentParser(
+***REMOVED***description="Create an App Registration and client secret (if not already created)",
+***REMOVED***epilog="Example: auth_update.py",
+***REMOVED***)
+***REMOVED***parser.add_argument(
+***REMOVED***"--appid",
+***REMOVED***required=False,
+***REMOVED***help="Optional. ID of registered application. If provided, this script just makes sure it exists.",
+***REMOVED***)
+***REMOVED***args = parser.parse_args()
+
+***REMOVED***credential = AzureDeveloperCliCredential()
+
+***REMOVED***if args.appid:
+***REMOVED***print(f"Checking if application {args.appid} exists")
+***REMOVED***if check_for_application(credential, args.appid):
+***REMOVED******REMOVED***print("Application already exists, not creating new one.")
+***REMOVED******REMOVED***exit(0)
+***REMOVED***
+***REMOVED***print("Creating application registration")
+***REMOVED***app_id, client_id = create_application(credential)
+
+***REMOVED***print(f"Adding client secret to {app_id}")
+***REMOVED***client_secret = add_client_secret(credential, app_id)
+***REMOVED***
+***REMOVED***print("Updating azd env with AUTH_APP_ID, AUTH_CLIENT_ID, AUTH_CLIENT_SECRET")
+***REMOVED***update_azd_env("AUTH_APP_ID", app_id)
+***REMOVED***update_azd_env("AUTH_CLIENT_ID", client_id)
+***REMOVED***update_azd_env("AUTH_CLIENT_SECRET", client_secret)
+
--- scripts/auth_update.py
+++ scripts/auth_update.py
@@ -1,11 +1,14 @@
 import argparse
-import subprocess
 
 from azure.identity import AzureDeveloperCliCredential
 import urllib3
 
 
 def update_redirect_uris(credential, app_id, uri):
+***REMOVED***redirect_uris = [
+***REMOVED******REMOVED******REMOVED***"http://localhost:5000/.auth/login/aad/callback",
+***REMOVED******REMOVED******REMOVED***f"{uri}/.auth/login/aad/callback",
+***REMOVED******REMOVED***]
 ***REMOVED***urllib3.request(
 ***REMOVED***"PATCH",
 ***REMOVED***f"https://graph.microsoft.com/v1.0/applications/{app_id}",
@@ -15,10 +18,7 @@ def update_redirect_uris(credential, app_id, uri):
 ***REMOVED***,
 ***REMOVED***json={
 ***REMOVED******REMOVED***"web": {
-***REMOVED******REMOVED***"redirectUris": [
-***REMOVED******REMOVED******REMOVED***"http://localhost:5000/.auth/login/aad/callback",
-***REMOVED******REMOVED******REMOVED***f"{uri}/.auth/login/aad/callback",
-***REMOVED******REMOVED***]
+***REMOVED******REMOVED***"redirectUris": redirect_uris
 ***REMOVED***
 ***REMOVED***,
 ***REMOVED***)
@@ -26,8 +26,8 @@ def update_redirect_uris(credential, app_id, uri):
 
 if __name__ == "__main__":
 ***REMOVED***parser = argparse.ArgumentParser(
-***REMOVED***description="Prepare documents by extracting content from PDFs, splitting content into sections and indexing in a search index.",
-***REMOVED***epilog="Example: prepdocs.py --searchservice mysearch --index myindex",
+***REMOVED***description="Add a redirect URI to a registered application",
+***REMOVED***epilog="Example: auth_update.py --appid 123 --uri https://abc.azureservices.net",
 ***REMOVED***)
 ***REMOVED***parser.add_argument(
 ***REMOVED***"--appid",
@@ -42,4 +42,6 @@ if __name__ == "__main__":
 ***REMOVED***args = parser.parse_args()
 
 ***REMOVED***credential = AzureDeveloperCliCredential()
+
+***REMOVED***print(f"Updating application registration {args.appid} with redirect URI for {args.uri}")
 ***REMOVED***update_redirect_uris(credential, args.appid, args.uri)
