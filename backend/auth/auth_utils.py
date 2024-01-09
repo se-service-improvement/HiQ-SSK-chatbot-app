963b2bf6f967c2a73174240b9cf1bb866966a038
Christoph Goessmann <44035999+gochristoph@users.noreply.github.com>
2024-01-09 02:10:59 +0100

    Remove unused AAD token to support other identity providers (#459)

--- backend/auth/auth_utils.py
+++ backend/auth/auth_utils.py
@@ -10,11 +10,11 @@ def get_authenticated_user_details(request_headers):
 ***REMOVED***## if it is, get the user details from the EasyAuth headers
 ***REMOVED***raw_user_object = {k:v for k,v in request_headers.items()}
 
-***REMOVED***user_object['user_principal_id'] = raw_user_object['X-Ms-Client-Principal-Id']
-***REMOVED***user_object['user_name'] = raw_user_object['X-Ms-Client-Principal-Name']
-***REMOVED***user_object['auth_provider'] = raw_user_object['X-Ms-Client-Principal-Idp']
-***REMOVED***user_object['auth_token'] = raw_user_object['X-Ms-Token-Aad-Id-Token']
-***REMOVED***user_object['client_principal_b64'] = raw_user_object['X-Ms-Client-Principal']
-***REMOVED***user_object['aad_id_token'] = raw_user_object["X-Ms-Token-Aad-Id-Token"]
+***REMOVED***user_object['user_principal_id'] = raw_user_object.get('X-Ms-Client-Principal-Id')
+***REMOVED***user_object['user_name'] = raw_user_object.get('X-Ms-Client-Principal-Name')
+***REMOVED***user_object['auth_provider'] = raw_user_object.get('X-Ms-Client-Principal-Idp')
+***REMOVED***user_object['auth_token'] = raw_user_object.get('X-Ms-Token-Aad-Id-Token')
+***REMOVED***user_object['client_principal_b64'] = raw_user_object.get('X-Ms-Client-Principal')
+***REMOVED***user_object['aad_id_token'] = raw_user_object.get('X-Ms-Token-Aad-Id-Token')
 
 ***REMOVED***return user_object
