dc7c2c569c6e0ee60eca39c17afad4fa6da55d9b
Slava Reznitsky <30771358+slreznit@users.noreply.github.com>
2024-09-12 17:58:12 +0300

    Pass ApplicationName to Microsoft Defender for Cloud (#1061)

--- app.py
+++ app.py
@@ -239,8 +239,9 @@ def prepare_model_args(request_body, request_headers):
 ***REMOVED***user_json = None
 ***REMOVED***if (MS_DEFENDER_ENABLED):
 ***REMOVED***authenticated_user_details = get_authenticated_user_details(request_headers)
-***REMOVED***conversation_id = request_body.get("conversation_id", None)***REMOVED***
-***REMOVED***user_json = get_msdefender_user_json(authenticated_user_details, request_headers, conversation_id)
+***REMOVED***conversation_id = request_body.get("conversation_id", None)
+***REMOVED***application_name = app_settings.ui.title
+***REMOVED***user_json = get_msdefender_user_json(authenticated_user_details, request_headers, conversation_id, application_name)
 
 ***REMOVED***model_args = {
 ***REMOVED***"messages": messages,
