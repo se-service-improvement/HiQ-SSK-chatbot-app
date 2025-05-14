551fb4ef8e5433e6e4af879daa858f17aad268ac
se-service-improvement <se.service.improvement@qut.edu.au>
2025-05-14 16:05:56 +1000

    FAQ Base Working

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 import { initializeIcons } from '@fluentui/react'
 
 import Chat from './pages/chat/Chat'
+import FAQ from './pages/faq/FAQ'
 import Layout from './pages/layout/Layout'
 import NoPage from './pages/NoPage'
 import { AppStateProvider } from './state/AppProvider'
@@ -19,6 +20,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Chat />} />
+            <Route path="faq/:faq_id" element={<FAQ />} />
             <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
